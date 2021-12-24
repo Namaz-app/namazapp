d221dcfdd37da1843f966dd24af7e029d3412d98
Abdurahman Adilovic <abdurahman.adilovic@gmail.com>
2021-12-24 21:36:13 +0100

    add new icons and titles for home tabs

--- app/src/main/java/ba/aadil/namaz/MainActivity.kt
+++ app/src/main/java/ba/aadil/namaz/MainActivity.kt
@@ -1,13 +1,13 @@
 package ba.aadil.namaz
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import ba.aadil.namaz.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,8 +24,13 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_prayer_tab,
+                R.id.navigation_dashboard_tab,
+                R.id.navigation_tracking_tab
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
