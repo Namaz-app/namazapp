04f9516cb05082243d6349cf48f2ba4e49b9f1f8
Abdurahman Adilovic <abdurahman.adilovic@gmail.com>
2021-12-26 15:00:43 +0100

    add a day to end date since startOfDay() will not calculate correctly prayed prayers for that day

--- app/src/main/java/ba/aadil/namaz/stats/GetStatisticsUseCase.kt
+++ app/src/main/java/ba/aadil/namaz/stats/GetStatisticsUseCase.kt
@@ -7,12 +7,15 @@ import java.time.ZoneOffset
 
 class GetStatisticsUseCase(private val trackingDao: TrackingDao) {
     fun getStatsBetweenDays(startDate: LocalDate, endDate: LocalDate): PrayerStatistics {
+        val endDatePlusDay = endDate.plusDays(1)
+
         val prayed = trackingDao.getAllCompletedPrayersBetweenTwoDates(
             startDate.atStartOfDay().toEpochSecond(ZoneOffset.ofTotalSeconds(0)),
-            endDate.atStartOfDay().toEpochSecond(ZoneOffset.ofTotalSeconds(0))
+            endDatePlusDay.atStartOfDay().toEpochSecond(ZoneOffset.ofTotalSeconds(0))
         )
         val totalPrayers =
-            Duration.between(startDate.atStartOfDay(), endDate.atStartOfDay()).toDays().toInt() * 5
+            Duration.between(startDate.atStartOfDay(), endDatePlusDay.atStartOfDay()).toDays()
+                .toInt() * 5
 
         return PrayerStatistics(prayed.size, totalPrayers)
     }
