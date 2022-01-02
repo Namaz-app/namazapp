0eec81296854a653571002e7187b4aafc4ce97a9
Abdurahman Adilovic <abdurahman.adilovic@gmail.com>
2022-01-02 20:25:08 +0100

    add multiple backstack support for navigation, fix koin crash, add jitpack maven repo

--- app/src/main/java/ba/aadil/namaz/NamazApplication.kt
+++ app/src/main/java/ba/aadil/namaz/NamazApplication.kt
@@ -7,13 +7,14 @@ import ba.aadil.namaz.di.presentationModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class NamazApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
         startKoin {
-            androidLogger()
+            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
             androidContext(this@NamazApplication)
             modules(dataModule, domainModule, presentationModule)
         }
