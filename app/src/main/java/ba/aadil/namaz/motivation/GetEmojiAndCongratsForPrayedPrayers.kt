5727e97e8fc6d32139ce3bd1f23d4744370fe908
Abdurahman Adilovic <abdurahman.adilovic@gmail.com>
2022-01-08 21:10:59 +0100

    add more motivational messages, fix message width

--- app/src/main/java/ba/aadil/namaz/motivation/GetEmojiAndCongratsForPrayedPrayers.kt
+++ app/src/main/java/ba/aadil/namaz/motivation/GetEmojiAndCongratsForPrayedPrayers.kt
@@ -5,7 +5,7 @@ import ba.aadil.namaz.R
 class GetEmojiAndCongratsForPrayedPrayers {
     fun get(prayedCount: Int): Pair<Int, String> {
         return when (prayedCount) {
-            0 -> Pair(R.string.congrats_gods_blessings2, "\uD83D\uDE1E")
+            0 -> Pair(R.string.congrats_gods_blessings3, "\uD83D\uDE1E")
             1, 2 -> Pair(R.string.congrats_gods_blessings2, "\uD83D\uDE10")
             3, 4 -> Pair(R.string.congrats_gods_blessings, "☺️")
             else -> Pair(R.string.congrats_gods_blessings, "\uD83E\uDD29")
