a37daa83555385fb708b34622315d58e080ceef7
Abdurahman Adilovic <abdurahman.adilovic@gmail.com>
2022-01-31 21:52:17 +0100

    setup settings view

---
+++ app/src/main/java/ba/aadil/namaz/ui/settings/SettingsFragment.kt
@@ -0,0 +1,48 @@
+package ba.aadil.namaz.ui.settings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import ba.aadil.namaz.R
+import ba.aadil.namaz.databinding.FragmentSettingsBinding
+import com.github.vivchar.rendererrecyclerviewadapter.RendererRecyclerViewAdapter
+import com.github.vivchar.rendererrecyclerviewadapter.ViewFinder
+import com.github.vivchar.rendererrecyclerviewadapter.ViewModel
+import com.github.vivchar.rendererrecyclerviewadapter.ViewRenderer
+
+class SettingsFragment : Fragment() {
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val rvAdapter = RendererRecyclerViewAdapter()
+        rvAdapter.registerRenderer(
+            ViewRenderer<NotificationsRow, ViewFinder>(
+                R.layout.notifications_row,
+                NotificationsRow::class.java
+            ) { model, finder, _ ->
+            })
+
+        binding.settingsRv.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = rvAdapter
+        }
+    }
+
+    data class NotificationsRow(val notifications: Boolean) : ViewModel
+    data class RemindersRow(val reminderTime: Int) : ViewModel
+    object SocialsRow : ViewModel
+}
