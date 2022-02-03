e8cbf367c0806a8457424b62458514c9e9d84350
Abdurahman Adilovic <abdurahman.adilovic@gmail.com>
2022-02-03 08:21:01 +0100

    add reminders spinner

--- app/src/main/java/ba/aadil/namaz/ui/settings/SettingsFragment.kt
+++ app/src/main/java/ba/aadil/namaz/ui/settings/SettingsFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import ba.aadil.namaz.R
@@ -38,16 +41,44 @@ class SettingsFragment : Fragment() {
                 finder.setChecked(R.id.notifications_toggle, model.notifications)
             })
 
+        val spinnerAdapter = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.reminder_times,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+        }
+        val spinnerListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+
+        rvAdapter.registerRenderer(ViewRenderer<RemindersRow, ViewFinder>(
+            R.layout.reminders_row,
+            RemindersRow::class.java
+        ) { model, finder, _ ->
+            finder.find<Spinner>(R.id.reminders_timer).apply {
+                adapter = spinnerAdapter
+                onItemSelectedListener = spinnerListener
+                setSelection(model.selectedIndex)
+            }
+        })
+
         binding.settingsRv.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = rvAdapter
         }
 
-        rvAdapter.setItems(listOf(NotificationsRow(true)))
+        rvAdapter.setItems(listOf(NotificationsRow(true), RemindersRow(selectedIndex = 2)))
         rvAdapter.notifyDataSetChanged()
     }
 
     data class NotificationsRow(val notifications: Boolean) : ViewModel
-    data class RemindersRow(val reminderTime: Int) : ViewModel
+    data class RemindersRow(val selectedIndex: Int) : ViewModel
     object SocialsRow : ViewModel
 }
