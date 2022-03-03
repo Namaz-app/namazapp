70022a0be4ffa3d28a87ff9483f4d13ad02dd847
Abdurahman Adilovic <abdurahman.adilovic@gmail.com>
2022-03-03 08:38:49 +0100

    cleanup

--- app/src/main/java/ba/aadil/namaz/MainActivity.kt
+++ app/src/main/java/ba/aadil/namaz/MainActivity.kt
@@ -9,15 +9,11 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import ba.aadil.namaz.databinding.ActivityMainBinding
 import ba.aadil.namaz.ui.auth.AuthActivity
-import ba.aadil.namaz.user.GetCurrentUser
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
-import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-    private val getCurrentUser by inject<GetCurrentUser>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
